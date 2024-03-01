7d60f6bf6f96c9fdc2c9b603059511cf3d18bbc6
Reece <reecejrobertson@gmail.com>
2024-03-01 10:39:54 -0500

    First pass at unified plotting function.

--- plotting.py
+++ plotting.py
@@ -18,133 +18,74 @@ ariaSimError = np.zeros(11)
 harmonySimError = np.zeros(11)
 qubitCounts = np.arange(4, 25, 2)
 
-for filename in os.listdir(brisbaneDir):
-    
-    numQubits = int(filename[1:filename.find('l')])
-    secretKey = '1' * numQubits
+def plotLine(directory, isReversed, isProbabilityFormat, shots, lineStyle, label):
 
-    file = os.path.join(brisbaneDir, filename)
-    with open(file, 'r') as fin:
-        data = fin.read()
+    errorArray = np.zeros(100)
+    iterations = 0
+    for filename in os.listdir(directory):
+        
+        numQubits = int(filename[1:filename.find('i')])
+        secretKey = '1' * numQubits
 
-    data = data.replace('\n', '').replace('\t', '')[1:-1]
-    dataDict = ast.literal_eval(data)
+        iteration = int(filename[filename.find('i')+1:-4])
+        if iteration > iterations:
+            iterations = iteration
 
-    for key in dataDict:
-        assert len(key) == 2*numQubits
-        break
+        file = os.path.join(directory, filename)
+        with open(file, 'r') as fin:
+            data = fin.read()
 
-    probDict = dict()
-    probSum = 0 
+        countsDict = None
 
-    for key, value in dataDict.items():
-        if value < 0:
-            probDict[key] = 0
+        if isProbabilityFormat:
+            data = data.replace('\n', '').replace('\t', '')[1:-1]
+            dataDict = ast.literal_eval(data)
+
+            for key in dataDict:
+                assert len(key) == 2*numQubits
+                break
+
+            probDict = dict()
+            probSum = 0 
+
+            for key, value in dataDict.items():
+                if value < 0:
+                    probDict[key] = 0
+                else:
+                    probDict[key] = value
+                    probSum += value
+
+            countsDict = dict()
+            countsSum = 0
+
+            for key, value in probDict.items():
+                count = round((value / probSum) * shots)
+                countsDict[key] = count
+                countsSum += count
+
         else:
-            probDict[key] = value
-            probSum += value
-
-    countsDict = dict()
-    countsSum = 0
-
-    for key, value in probDict.items():
-        count = round((value / probSum) * shots)
-        countsDict[key] = count
-        countsSum += count
-
-    errorCount = 0
-
-    for key, value in countsDict.items():
-        actualKey = key[::-1][:numQubits]
-        isOrthogonal = (sum([int(actualKey[i]) * int(secretKey[i]) for i in range(numQubits)]) % 2) == 0
-        if not isOrthogonal:
-            errorCount += value
-
-    brisbaneError[int(numQubits) - 2] += errorCount
-
-brisbaneError /= shots * brisbaneReps
-
-for filename in os.listdir(brisbaneSimDir):
-
-    optimization = int(filename[filename.find('l')+1:filename.find('i')])
-    if optimization == 1:
-        numQubits = int(filename[1:filename.find('l')])
-        secretKey = '1' * numQubits
-
-        file = os.path.join(brisbaneSimDir, filename)
-        with open(file, 'r') as fin:
-            data = fin.read()
-
-        countsDict = ast.literal_eval(data)
+            countsDict = ast.literal_eval(data)
 
         errorCount = 0
 
         for key, value in countsDict.items():
-            actualKey = key[::-1][:numQubits]
-            isOrthogonal = (sum([int(actualKey[i]) * int(secretKey[i]) for i in range(numQubits)]) % 2) == 0
+            if isReversed:
+                key = key[::-1]
+            key = key[:numQubits]
+            isOrthogonal = (sum([int(key[i]) * int(secretKey[i]) for i in range(numQubits)]) % 2) == 0
             if not isOrthogonal:
                 errorCount += value
 
-        brisbaneSimError[int(numQubits) - 2] += errorCount
+        errorArray[int(numQubits) - 2] += errorCount
 
-brisbaneSimError /= (shots * brisbaneRepetitions)
+    errorArray /= shots * iterations
+    errorArray = np.trim_zeros(errorArray)
 
-for filename in os.listdir(ariaSimDir):
+    qubitCounts = np.arange(4, 2*len(errorArray), 2)
 
-    mitigation = int(filename[-7])
-    sharpening = int(filename[-5])
-    if mitigation == 0 and sharpening == 0:
-        numQubits = int(filename[1:filename.find('i')])
-        secretKey = '1' * numQubits
+    plt.plot(qubitCounts, errorArray*100, lineStyle, label=label)
 
-        file = os.path.join(ariaSimDir, filename)
-        with open(file, 'r') as fin:
-            data = fin.read()
 
-        countsDict = ast.literal_eval(data)
-
-        errorCount = 0
-
-        for key, value in countsDict.items():
-            actualKey = key[numQubits:]
-            isOrthogonal = (sum([int(actualKey[i]) * int(secretKey[i]) for i in range(numQubits)]) % 2) == 0
-            if not isOrthogonal:
-                errorCount += value
-
-        ariaSimError[int(numQubits) - 2] += errorCount
-
-ariaSimError /= (shots * ionqRepetitions)
-
-for filename in os.listdir(harmonySimDir):
-
-    mitigation = int(filename[-7])
-    sharpening = int(filename[-5])
-    if mitigation == 0 and sharpening == 0:
-        numQubits = int(filename[1:filename.find('i')])
-        secretKey = '1' * numQubits
-
-        file = os.path.join(harmonySimDir, filename)
-        with open(file, 'r') as fin:
-            data = fin.read()
-
-        countsDict = ast.literal_eval(data)
-
-        errorCount = 0
-
-        for key, value in countsDict.items():
-            actualKey = key[numQubits:]
-            isOrthogonal = (sum([int(actualKey[i]) * int(secretKey[i]) for i in range(numQubits)]) % 2) == 0
-            if not isOrthogonal:
-                errorCount += value
-
-        harmonySimError[int(numQubits) - 2] += errorCount
-
-harmonySimError /= (shots * ionqRepetitions)
-
-plt.plot(qubitCounts, brisbaneError*100, '.-', label='Brisbane')
-plt.plot(qubitCounts[:7], brisbaneSimError[:7]*100, '.-', label='Brisbane Simulator')
-# plt.plot(qubitCounts, ariaSimError*100, '.-', label='Aria Simulator')
-# plt.plot(qubitCounts[:4], harmonySimError[:4]*100, '.-', label='Harmony Simulator')
 plt.xticks(qubitCounts)
 plt.xlabel('Number of Qubits')
 plt.ylabel('Percentage of Invalid Outputs')
